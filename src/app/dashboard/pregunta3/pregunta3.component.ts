454376c361e913dc4fc7434839ff2ecff7b4af48
AlexHc26 <mahc2601@gmail.com>
2024-06-22 21:21:57 -0500

    3pregunta segundo commit

--- src/app/dashboard/pregunta3/pregunta3.component.ts
+++ src/app/dashboard/pregunta3/pregunta3.component.ts
@@ -2,11 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pregunta3',
-  standalone: true,
-  imports: [],
   templateUrl: './pregunta3.component.html',
-  styleUrl: './pregunta3.component.css'
+  styleUrls: ['./pregunta3.component.css']
 })
 export class Pregunta3Component {
+  monto: number = 0;
+  cuotas: number = 0;
+  interes: number = 0;
+  total: number = 0;
+  montoCuota: number = 0;
 
+  calcularCuotas() {
+    if (this.monto > 5000) {
+      this.cuotas = 3;
+      this.interes = 0.10;
+    } else if (this.monto < 1000) {
+      this.cuotas = 1;
+      this.interes = 0.12;
+    } else if (this.monto >= 1000 && this.monto <= 5000) {
+      this.cuotas = 2;
+      this.interes = 0.12;
+    } else {
+      this.cuotas = 5;
+      this.interes = 0.12;
+    }
+
+    this.total = this.monto * (1 + this.interes);
+    this.montoCuota = this.total / this.cuotas;
+  }
 }
