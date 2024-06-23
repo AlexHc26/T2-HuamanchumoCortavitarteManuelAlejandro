4499805f1f87095ff3e391a0d27e926df6991f8a
AlexHc26 <mahc2601@gmail.com>
2024-06-22 21:28:15 -0500

    4pregunta segundo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { Pregunta2Component } from './dashboard/pregunta2/pregunta2.component';
+import { Pregunta3Component } from './dashboard/pregunta3/pregunta3.component';
+import { Pregunta4Component } from './dashboard/pregunta4/pregunta4.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], children: [
+      { path: 'pregunta2', component: Pregunta2Component },
+      { path: 'pregunta3', component: Pregunta3Component },
+      { path: 'pregunta4', component: Pregunta4Component }
+    ] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
--- src/app/dashboard/pregunta4/pregunta4.component.ts
+++ src/app/dashboard/pregunta4/pregunta4.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pregunta4',
-  standalone: true,
-  imports: [],
   templateUrl: './pregunta4.component.html',
-  styleUrl: './pregunta4.component.css'
+  styleUrls: ['./pregunta4.component.css']
 })
 export class Pregunta4Component {
+  horas: number = 0;
+  salario: number = 0;
 
+  calcularSalario() {
+    if (this.horas <= 40) {
+      this.salario = this.horas * 16;
+    } else {
+      this.salario = (40 * 16) + ((this.horas - 40) * 20);
+    }
+  }
 }
